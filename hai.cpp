89e52373316aeca0338ffb42cc0ef7028bf44651
Nguyễn Đại Khang <111965121+khanghaicode@users.noreply.github.com>
2024-09-10 23:58:41 +0700

    Update hai.cpp -timerun

--- hai.cpp
+++ hai.cpp
@@ -1,19 +1,22 @@
-/*khanghaicode da lolihunter*/
+/*khanghaicode da loiihunter*/
+/*--------------------------*/
 /*ai doc duoc la gay*/
+/*khong code thi thoi t la mot con cho*/
 #include<bits/stdc++.h>
 #define ll long long
 using namespace std;
-const ll mod=1e9+7;
 void process(){
-
+    
 }
 void init(){
     
 }
 int main(){
-    ios_base::sync_with_stdio(0);cin.tie(0);
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     freopen("input.inp","r",stdin);
     freopen("output.out","w",stdout);
+    clock_t begin=clock();
     init();
-    process();
+    process();   
+    cout<<"\n"<<(float)(clock()-begin)/CLOCKS_PER_SEC<<"s";
 }
